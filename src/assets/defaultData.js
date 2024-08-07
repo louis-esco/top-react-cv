de47e3e7e98f7518d2acbda99572ad938cb26a85
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-08-07 18:47:20 +0200

    Add datasets

---
+++ src/assets/defaultData.js
@@ -0,0 +1,13 @@
+export const defaultData = {
+  name: "Louis Escoffier",
+  mail: "louis.escoffier@gmail.com",
+  phone: "06 00 00 00 00",
+  school: "INSA Rennes",
+  studyTitle: "Génie Mécanique et Automatismes",
+  studyDate: "2013 - 2018",
+  company: "Silbo",
+  position: "Head of Product",
+  responsibilities: "In charge of the product team",
+  started: "2013",
+  ended: "2024",
+};
---
+++ src/assets/inputsLists.js
@@ -0,0 +1,21 @@
+export { identity, education, experience };
+
+const identity = [
+  { id: "name", value: "Name:" },
+  { id: "mail", value: "Mail:" },
+  { id: "phone", value: "Phone:" },
+];
+
+const education = [
+  { id: "school", value: "School name:" },
+  { id: "studyTitle", value: "Title of study:" },
+  { id: "studyDate", value: "Date of study:" },
+];
+
+const experience = [
+  { id: "company", value: "Company:" },
+  { id: "position", value: "Position:" },
+  { id: "responsibilities", value: "Main responsibilities:" },
+  { id: "started", value: "Started in:" },
+  { id: "ended", value: "Ended in:" },
+];
