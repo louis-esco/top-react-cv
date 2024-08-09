0d900f265e0a7fa500d8ea44cd9b0914ca40e351
Louis Escoffier <louis.escoffier.bagrel@gmail.com>
2024-08-09 19:04:48 +0200

    Add inputType prop in form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import InputSection from "./InputSection";
-import { identity, education, experience } from "../assets/inputsLists";
+import { identity, education, experience } from "../data/inputsLists";
 
 function Form({ formData, setFormData }) {
   return (
@@ -9,18 +9,21 @@ function Form({ formData, setFormData }) {
         fields={identity}
         data={formData}
         setFormData={setFormData}
+        inputsType="auto"
       />
       <InputSection
         title="Educational experience"
         fields={education}
         data={formData}
         setFormData={setFormData}
+        inputsType="auto"
       />
       <InputSection
         title="Practical experience"
         fields={experience}
         data={formData}
         setFormData={setFormData}
+        inputsType="manual"
       />
     </div>
   );
